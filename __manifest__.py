02d5ea8f0d6d8a8a20b3431f4dce54d9f508cc09
Gonzalo Zelaya <gonzalo.zelaya@outsourcearg.com>
2025-02-05 23:03:15 -0300

    Updated module

--- __manifest__.py
+++ __manifest__.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "fix_pos",
+    'name': "pos_custom_stock_reduction",
 
     'summary': """
-        Quick workaround to a POS problem where you can't close session because it's creating a payment with the same account in liquidity and counterpart""",
+        Allows custom stock reduction in POS according to client needs""",
 
     'description': """
-        Quick workaround to a POS problem where you can't close session because it's creating a payment with the same account in liquidity and counterpart
+        This module allows for custom stock reduction in the Point of Sale (POS) system according to the specific needs of the client. It provides flexibility in managing inventory by enabling customized rules and conditions for stock deduction when sales are made through the POS.
     """,
 
     'author': "OutsourceArg",
-    'website': "http://www.yourcompany.com",
+    'website': "http://www.outsourcearg.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
